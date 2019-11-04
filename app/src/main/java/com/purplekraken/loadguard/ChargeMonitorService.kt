8143071101f95c44e19a6a3905d412d45444ee6c
Florian Limberger <flo@purplekraken.com>
2019-11-04 13:29:03 +0100

    Update monitor service notification
    Display and update the battery level as notification title.

--- app/src/main/java/com/purplekraken/loadguard/ChargeMonitorService.kt
+++ app/src/main/java/com/purplekraken/loadguard/ChargeMonitorService.kt
@@ -1,15 +1,13 @@
 package com.purplekraken.loadguard
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class ChargeMonitorService : Service() {
     companion object {
@@ -31,12 +29,17 @@ class ChargeMonitorService : Service() {
     private val batteryListener: BatteryUpdateCallback = {
         if (!it.isCharging) {
             doStopSelf()
+        } else {
+            updateNotification(it.chargingLevel)
         }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        (application as LoadGuardApp).batteryMonitor.addListener(batteryListener)
-        startForeground(SVC_NOTIFICATION_ID, createNotification())
+        val batmon = (application as LoadGuardApp).batteryMonitor
+        batmon.addListener(batteryListener)
+        val lvl = batmon.chargingLevel
+        startForeground(SVC_NOTIFICATION_ID, createNotification(lvl))
+        lastBatteryLevel = lvl
         Log.d(TAG, "service started")
         return super.onStartCommand(intent, flags, startId)
     }
@@ -55,13 +58,38 @@ class ChargeMonitorService : Service() {
         }
     }
 
-    private fun createNotification(): Notification {
-        return NotificationCompat.Builder(this, SVC_CHANNEL_ID)
-            .setContentTitle(getString(R.string.notification_title))
-            .setContentText(getString(R.string.notification_text))
-            .setSmallIcon(R.drawable.ic_shield_half_full_black_48dp)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+    private var notificationBuilder: NotificationCompat.Builder? = null
+
+    private fun createNotification(chargingLevel: Int): Notification {
+        val builder = notificationBuilder ?: {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+            val builder = NotificationCompat.Builder(this, SVC_CHANNEL_ID)
+                .setContentText(getString(R.string.notification_text))
+                .setSmallIcon(R.drawable.ic_shield_half_full_white_48dp)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent)
+            notificationBuilder = builder
+            builder
+        }()
+        return builder
+            .setContentTitle("$chargingLevel %")
             .build()
+    }
+
+    private var lastBatteryLevel = 0
+
+    private fun updateNotification(chargingLevel: Int) {
+        if (lastBatteryLevel != chargingLevel) {
+            val n = createNotification(chargingLevel)
+            with(NotificationManagerCompat.from(this)) {
+                notify(SVC_NOTIFICATION_ID, n)
+            }
+            lastBatteryLevel = chargingLevel
+        }
     }
 
     // Call this instead of `stopSelf()` to clean up properly
